2750b75cb3490b61c73cf6ee0537d439d97c8dde
Vinoj Kannan <vinoj.kannan@samsung.com>
2020-08-27 18:47:47 +0530

    nuxt ssg changes

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1 style="text-align: center">VUE SSG - Jokes</h1>
+    <v-container>
+      <v-list three-line>
+        <template v-for="(category, index) in categories">
+          <v-divider :key="index"></v-divider>
+          <v-list-item :key="category">
+            <v-list-item-content>
+              <v-list-item-title>
+                <NuxtLink :to="{ path: `jokes/${category}` }">
+                  {{ category }}
+                </NuxtLink>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ $content, params }) {
+    // const doc = await $content('hello').fetch()
+    // return { doc }
+    const { data } = await axios.get(
+      'https://api.chucknorris.io/jokes/categories'
+    )
+    return { categories: data }
+  },
+}
+</script>
---
+++ pages/jokes/_category.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 style="padding: 100px">{{ joke.value }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ $content, params }) {
+    // const doc = await $content('hello').fetch()
+    // return { doc }
+    const { data } = await axios.get(
+      `https://api.chucknorris.io/jokes/random?category=${params.category}`
+    )
+    return { joke: data }
+  },
+}
+</script>
